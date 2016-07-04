4c5b4fd8fd5b04e21fb9620496759cab9e36ad04
Erik Paulson <epaulson10@gmail.com>
2016-07-04 15:05:17 -0700

    Simple messaging implemented
    A simple form of messaging with a message window, a composition box, and a send button.

--- chat.js
+++ chat.js
@@ -1,11 +1,21 @@
 var http = require('http');
 var chat = {};
-var Server = require('socket.io');
-var io = Server();
+var io = require('socket.io')();
 chat.io = io;
 
-io.on('connection', function() {
+io.on('connection', function(socket) {
     console.log("Someone connected");
+    socket.nick = "Anonymous";
+
+    socket.on('NICK', function(nickname) {
+        socket.nick = nickname;
+        // Need to broadcast this to everyone else somehow
+    });
+
+    socket.on('text message', function(message) {
+        console.log("Received message:" +message.message);
+        io.emit('text message', message);
+    });
 });
 
 module.exports = chat;
