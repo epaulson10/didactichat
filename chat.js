a52aeb549d4eb4d1255e671a4f0133f54c294141
Erik Paulson <epaulson10@gmail.com>
2016-07-24 16:38:56 -0700

    Initial implementation of multiroom chat
    At this point, all rooms are public. The blue room is the active room, and red text indicates new messages.

--- chat.js
+++ chat.js
@@ -3,9 +3,13 @@ var chat = {};
 var io = require('socket.io')();
 chat.io = io;
 
+var roomList = [];
+
 io.on('connection', function(socket) {
     console.log("Someone connected");
     socket.nick = "Anonymous";
+    socket.room = "chat";
+    socket.emit('room list', roomList);
 
     socket.on('NICK', function(nickname) {
         var old = socket.nick;
@@ -14,8 +18,20 @@ io.on('connection', function(socket) {
     });
 
     socket.on('text message', function(textMsg) {
-        console.log("Received message:" +textMsg.message);
-        io.emit('text message', {from: socket.nick, message: textMsg.message});
+        console.log("Received message: \"" +textMsg.message + "\" for room: " + textMsg.room);
+        if(textMsg.room){
+            io.sockets.in(textMsg.room).emit('text message', {from: socket.nick, message: textMsg.message, room: textMsg.room});
+        }
+    });
+
+    socket.on('join', function (joinRequest) {
+        console.log("Request to join room: " + joinRequest.room);
+        socket.join(joinRequest.room);
+        if (roomList.indexOf(joinRequest.room) === -1) {
+            console.log("Creating room: " + joinRequest.room);
+            roomList.push(joinRequest.room);
+            io.emit('room create', joinRequest.room);
+        }
     });
 });
 
--- public/chat_client.js
+++ public/chat_client.js
@@ -1,8 +1,11 @@
 var socket = io();
 
+var activeRoom = "lobby";;
+socket.emit('join', {room: "lobby"});
+
 function sendMessage() {
     var msgBox = $("#composeBox");
-    socket.emit('text message', { message: msgBox.val()});
+    socket.emit('text message', { message: msgBox.val(), room: activeRoom});
     msgBox.val("");
 };
 
@@ -12,9 +15,26 @@ function sendNick() {
     nickBox.val('');
 };
 
+function createRoom() {
+    var room = window.prompt("Enter room name");
+    if (room !== "") {
+        socket.emit('join', {room: room});
+    }
+}
+
+function addRoomClickHandler(room) {
+    $('#'+room + "list").click(function() {
+        $(this).css('color', 'blue');
+        $('#'+activeRoom + "list").css('color', 'black');
+        $('#'+activeRoom).hide();
+        activeRoom = $(this).text();
+        $('#'+activeRoom).show();
+    });
+}
+
 socket.on('text message', function(message) {
     var messageList = $('#messageList');
-    addTextToMsgWindow(message.from + ": " + message.message);
+    addTextToMsgWindow(message.from + ": " + message.message, message.room);
 });
 
 socket.on('NICK', function(nickChange) {
@@ -22,8 +42,49 @@ socket.on('NICK', function(nickChange) {
     addTextToMsgWindow(changeStr);
 });
 
-function addTextToMsgWindow(text) {
-    var msgList = $('#messageList');
+socket.on('room create', function(newRoom) {
+    $('#roomList').append("<li id=" + newRoom + "list><a>" + newRoom + "</a></li>");
+    $('#messageDiv').append("<ul id="+ newRoom + "></ul>");
+    if ($('#messageDiv').children().length > 1) {
+        $('#'+newRoom).hide();
+    } else {
+        $('#'+newRoom + "list").css('color', 'blue');
+    }
+
+    socket.emit('join', {room: newRoom});
+    addRoomClickHandler(newRoom);
+});
+
+
+socket.on('room list', function(roomList) {
+    for (var i = 0; i < roomList.length; i++) {
+        $('#roomList').append("<li id=" + roomList[i]+"list>" + roomList[i] + "</li>");
+        $('#messageDiv').append("<ul id="+roomList[i] + "></ul>");
+        addRoomClickHandler(roomList[i]);
+        socket.emit('join', {room: roomList[i]});
+        if (i == 0) {
+            $('#' + roomList[i] + 'list').css('color','blue');
+        } else {
+            $('#' + roomList[i]).hide();
+        }
+    }
+});
+
+function addTextToMsgWindow(text, room) {
+    var msgList;
+    var roomListElem;
+    if (room === undefined) {
+        room = activeRoom;
+        msgList = $('#' + activeRoom);
+        roomListElem = $('#' + activeRoom + 'list');
+
+    } else {
+        msgList = $('#' + room);
+        roomListElem = $('#' + room + 'list');
+    }
+    if (room != activeRoom) {
+        roomListElem.css('color', 'red');
+    }
     var msgDiv = $('#messageDiv');
     msgList.append("<li>" + text + "</li>");
     msgDiv.scrollTop(msgDiv.prop('scrollHeight'));
