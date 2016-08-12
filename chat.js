6456ada3d82425a4612a7e08d9b0f20e94ef63b4
Erik Paulson <epaulson10@gmail.com>
2016-08-11 18:55:09 -0700

    Add copyright headers to files

--- chat.js
+++ chat.js
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) Erik Paulson 2016
+ *
+ * This file is part of didactichat.
+ *
+ * didactichat is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * didactichat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with didactichat.  If not, see <http://www.gnu.org/licenses/>.
+ */
 var http = require('http');
 var chat = {};
 var io = require('socket.io')();
